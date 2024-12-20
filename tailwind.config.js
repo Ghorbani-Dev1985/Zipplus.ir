08cc0a57d10fa364dd051691c6b1b60aa29a8e1f
ghorbani-dev1985 <ghorbani.dev1985@gmail.com>
2024-12-20 13:27:41 +0330

    feat: add tailwind & colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,9 +12,27 @@ module.exports = {
           md: "0.625rem",
         },
       },
+      colors:{
+       primary: {
+          100: '#AC988D',
+          200: '#7F6051',
+          300: '#6A462F',
+          DEFAULT: '#4F2C19',
+           },
+       secondary: {
+          400: '#625C4B',
+          500: '#A57939',
+          DEFAULT: '#B0A27B',
+           },
+           success: 'rgb(0 , 192 , 115)',
+           background: '#F6F5F0',
+           warning: 'rgb(255 , 153 , 0)',
+           error: 'rgb(255,71 , 87)',
+           danger: '#f43f5e'
+       },
       fontFamily: {
-        ShabnamFD: ["Shabnam-FD", "Tahome"],
-        Shabnam: ["Shabnam", "Tahome"],
+        ShabnamFD: ["Shabnam-FD", "Tahoma"],
+        Shabnam: ["Shabnam", "Tahoma"],
       },
     },
   },
